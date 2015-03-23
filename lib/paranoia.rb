50aca4a32d5379496aa162818a2349d5ad1d0056
Sergey Gernyak <sergeg1990@gmail.com>
2015-03-23 12:08:01 +0200

    Manual defining default scope

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -140,7 +140,9 @@ class ActiveRecord::Base
     class_attribute :paranoia_column
 
     self.paranoia_column = options[:column] || :deleted_at
-    default_scope { where(self.quoted_table_name + ".#{paranoia_column} IS NULL") }
+    default_scope { where(self.quoted_table_name + ".#{paranoia_column} IS NULL") } if options.fetch(:default_scope, true)
+
+    scope :without_deleted, ->{ where(self.quoted_table_name + ".#{paranoia_column} IS NULL") }
 
     before_restore {
       self.class.notify_observers(:before_restore, self) if self.class.respond_to?(:notify_observers)
